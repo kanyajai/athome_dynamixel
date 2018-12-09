56753425b74aee13e01b59727ae50410bc7cbe72
kanyajai <k.yajai22@gmail.com>
2018-12-09 14:00:11 +0700

    update

--- src/commander/motor_1.py
+++ src/commander/motor_1.py
@@ -11,5 +11,5 @@ def command(pos):
     pub = rospy.Publisher('pan_controller_1_/command', Float64, queue_size=10)
     rospy.init_node('commander/motor_1', anonymous=True)
     pub.publish(pos)
-        
+
 
